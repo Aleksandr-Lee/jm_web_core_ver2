c2c8b746ea29839dc271b11e4335d4e1f4b2eb11
Aleksandr-Lee <71881684+Aleksandr-Lee@users.noreply.github.com>
2020-10-16 17:51:44 +0300

    Add files via upload

--- js/block-1-5.js
+++ js/block-1-5.js
@@ -12,17 +12,16 @@ var swiper = new Swiper('.swiper-container', {
 var clickBtn = document.querySelector('.button-show');
 var hidden = document.querySelector('.limit-height');
 var text = document.querySelector('.button-show__text');
-
+var textValue =  text.textContent;
 clickBtn.addEventListener('click', function () {
    if (hidden.style.maxHeight === '400px') {
 		hidden.style.maxHeight = '192px'
 	} else {
 		hidden.style.maxHeight = '400px'
 	}
-	
-	if (text.textContent === 'Показать все' ) {
+	if (text.textContent === textValue ) {
 		  text.textContent = 'Свернуть' 
 	} else {
-		  text.textContent = 'Показать все'
+		  text.textContent = textValue;
 	}
 });
